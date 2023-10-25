b14c394da30b6815e58ef0be5de4a99ada0b9155
Divesh Jadhav <diveshvjadhav@gmail.com>
2023-10-25 17:42:42 +0530

    Arguments Object

--- javascript/Day4/02_FunctionCalls.js
+++ javascript/Day4/02_FunctionCalls.js
@@ -2,16 +2,29 @@
 let a = 20;
 a = '20';
 
-console.log("Value of A : ",a)
-console.log("Type of A : ",typeof a)
+console.log("Value of A : ", a)
+console.log("Type of A : ", typeof a)
 
 //* Check for Function
-function sum(a,b){
+function sum(a, b) {
     return a + b;
 }
 
-console.log("Sum of 3 and 5 : ",sum(3,5));
-console.log("Sum of 2 : ",sum(2)); //* 1 + undefined //* o/p => NaN
-console.log("Sum : ",sum()); //* undefined + undefined //* o/p => NaN 
-console.log("Sum : ",sum(1,5,2,7,8,9)); //* other elements are neglected
+console.log("Sum of 3 and 5 : ", sum(3, 5));
+console.log("Sum of 2 : ", sum(2)); //* 1 + undefined //* o/p => NaN
+console.log("Sum : ", sum()); //* undefined + undefined //* o/p => NaN 
+console.log("Sum : ", sum(1, 5, 2, 7, 8, 9)); //* other elements are neglected
+
+//* Remaining Value of parameter in arguments
+function values(a, b) {
+    let total = 0;
+    for (let val in arguments) {
+        console.log("Val : ", val);
+    }
+    return a + b;
+}
+values(1, 2, 3, 4, 5, 5);
+console.log("Final Result : ", values(1, 5, 5));
+
+
 
